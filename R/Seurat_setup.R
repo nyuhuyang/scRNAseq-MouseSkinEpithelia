75a3eb0d38e759f3d2c0c2aad4d6fe2aee5c2d9e
Yang Hu <nyuhuyang@gmail.com>
2018-08-15 20:00:24 -0400

    intial run

--- R/Seurat_setup.R
+++ R/Seurat_setup.R
@@ -5,6 +5,7 @@
 # ######################################################################
 
 library(Seurat)
+library(SingleR)
 library(dplyr)
 library(ggrepel)
 source("../R/Seurat_functions.R")
@@ -19,39 +20,37 @@ source("../R/Seurat_functions.R")
 # setup Seurat objects since both count matrices have already filtered
 # cells, we do no additional filtering here
 
-# rename all "_" into "-" in sample names 
-SSCs_raw <- list()
-SSCs_Seurat <- list()
-samples <- c("Ad-depleteSp","Ad-Thy1","PND14","PND18","PND18pre",
-             "PND25","PND30","PND6" )
-conditions <- c("Adault-SSCs","Adault-SSCs","first-wave","first-wave",
-                "first-wave","first-wave","first-wave","first-wave" )
-
+MouseSkin_dgCMatrix <- Read10X(data.dir = paste0("./data/skin1/outs/raw_gene_bc_matrices/mm10/"))
+MouseSkin_raw <- list("young" = MouseSkin_dgCMatrix[,grepl("-1",colnames(MouseSkin_dgCMatrix))],
+                      "old" = MouseSkin_dgCMatrix[,grepl("-2",colnames(MouseSkin_dgCMatrix))])
+MouseSkin_Seurat <- list()
+samples <- c("young","old")
+conditions <- c("young","old")
+# 1 less, 2 more
+# 1 yound, 2 old
 for(i in 1:length(samples)){
-    SSCs_raw[[i]] <- Read10X(data.dir = paste0("./data/",
-                                                     samples[i],"/outs/filtered_gene_bc_matrices/mm10/"))
-    colnames(SSCs_raw[[i]]) <- paste0(samples[i],"_",colnames(SSCs_raw[[i]]))
-    SSCs_Seurat[[i]] <- CreateSeuratObject(SSCs_raw[[i]],
+    colnames(MouseSkin_raw[[i]]) <- paste0(samples[i],"_",colnames(MouseSkin_raw[[i]]))
+    MouseSkin_Seurat[[i]] <- CreateSeuratObject(MouseSkin_raw[[i]],
                                            min.cells = 3,
                                            min.genes = 200,
                                            names.delim = "_")
-    SSCs_Seurat[[i]]@meta.data$conditions <- conditions[i]
+    MouseSkin_Seurat[[i]]@meta.data$conditions <- conditions[i]
 }
-SSCs_Seurat <- lapply(SSCs_Seurat, FilterCells, 
+MouseSkin_Seurat <- lapply(MouseSkin_Seurat, FilterCells, 
                             subset.names = "nGene", 
                             low.thresholds = 200, 
-                            high.thresholds = Inf)
-SSCs_Seurat <- lapply(SSCs_Seurat, NormalizeData)
-SSCs_Seurat <- lapply(SSCs_Seurat, ScaleData)
-SSCs_Seurat <- lapply(SSCs_Seurat, FindVariableGenes, do.plot = FALSE)
+                            high.thresholds = Inf) %>% 
+        lapply(NormalizeData) %>% 
+        lapply(ScaleData) %>% 
+        lapply(FindVariableGenes, do.plot = FALSE)
 
 # we will take the union of the top 1k variable genes in each dataset for
 # alignment note that we use 1k genes in the manuscript examples, you can
 # try this here with negligible changes to the overall results
-genes.use <- lapply(SSCs_Seurat, function(x) head(rownames(x@hvg.info), 600))
+genes.use <- lapply(MouseSkin_Seurat, function(x) head(rownames(x@hvg.info), 1000))
 genes.use <- unique(unlist(genes.use))
 for(i in 1:length(samples)){
-        genes.use <- intersect(genes.use, rownames(SSCs_Seurat[[i]]@scale.data))
+        genes.use <- intersect(genes.use, rownames(MouseSkin_Seurat[[i]]@scale.data))
 }
 length(genes.use) # 1/10 of total sample size
 
@@ -59,100 +58,89 @@ length(genes.use) # 1/10 of total sample size
 #======1.2 Perform a canonical correlation analysis (CCA) =========================
 # run a canonical correlation analysis to identify common sources
 # of variation between the two datasets.
-remove(SSCs_raw)
-GC()
-SSCs <- RunMultiCCA(object.list = SSCs_Seurat, 
-                    genes.use = genes.use,
-                    niter = 25, num.ccs = 50,
-                    standardize =TRUE)
-save(SSCs, file = "./data/SSCs_alignment.Rda")
 
+MouseSkin <- RunCCA(MouseSkin_Seurat[[1]], MouseSkin_Seurat[[2]], 
+                    genes.use = genes.use,num.cc = 50)
+save(MouseSkin, file = "./data/MouseSkin_alignment.Rda")
+remove(MouseSkin_raw,MouseSkin_Seurat)
+GC()
 # Calculate median UMI per cell
-SSCs_raw_data <- as.matrix(x = SSCs@raw.data)
-mean(colSums(SSCs_raw_data))
-median(colSums(SSCs_raw_data))
-boxplot(colSums(SSCs_raw_data))
+MouseSkin_raw_data <- as.matrix(x = MouseSkin@raw.data)
+MouseSkin_young <- MouseSkin_raw_data[,grepl("young_",colnames(MouseSkin_raw_data))]
+MouseSkin_old <- MouseSkin_raw_data[,grepl("old_",colnames(MouseSkin_raw_data))]
+mean(colSums(MouseSkin_young)); mean(colSums(MouseSkin_old))
+median(colSums(MouseSkin_young)); median(colSums(MouseSkin_old))
+par(mfrow = c(1,2))
+boxplot(colSums(MouseSkin_young), ylim = c(0, 21000))
+title(main = "nUMI per cell in young mouse skin sample")
+boxplot(colSums(MouseSkin_old),ylim = c(0, 21000))
+title(main = "nUMI per cell in old mouse skin sample")
+remove(MouseSkin_raw_data,MouseSkin_young,MouseSkin_old)
+GC()
 # CCA plot CC1 versus CC2 and look at a violin plot
-p1 <- DimPlot(object = SSCs, reduction.use = "cca", group.by = "orig.ident", 
+p1 <- DimPlot(object = MouseSkin, reduction.use = "cca", group.by = "conditions", 
               pt.size = 0.5, do.return = TRUE)
-p2 <- VlnPlot(object = SSCs, features.plot = "CC1", group.by = "orig.ident", 
+p2 <- VlnPlot(object = MouseSkin, features.plot = "CC1", group.by = "conditions", 
               do.return = TRUE)
 plot_grid(p1, p2)
 
-AverageExpression()
-p3 <- MetageneBicorPlot(SSCs, grouping.var = "orig.ident", dims.eval = 1:50, 
-                        display.progress = TRUE, display.progress = FALSE,
-                        smooth = TRUE) # run on cluster
+PrintDim(object = MouseSkin, reduction.type = "cca", dims.print = 1:2, genes.print = 10)
+
+p3 <- MetageneBicorPlot(MouseSkin, grouping.var = "conditions", dims.eval = 1:50, 
+                        display.progress = TRUE, smooth = TRUE) # run on cluster
 p3 + geom_smooth(method = 'loess')
 
-PrintDim(object = SSCs, reduction.type = "cca", dims.print = 1:2, genes.print = 10)
+DimHeatmap(object = MouseSkin, reduction.type = "cca", cells.use = 500, dim.use = c(1:3,25:20), 
+           do.balanced = TRUE)
 
-DimHeatmap(object = SSCs, reduction.type = "cca", cells.use = 500, dim.use = c(1:9), 
-           do.balanced = TRUE)
-DimHeatmap(object = SSCs, reduction.type = "cca", cells.use = 500, dim.use = c(10:18), 
-           do.balanced = TRUE)
 #======1.3 QC =========================
-SSCs <- CalcVarExpRatio(object = SSCs, reduction.type = "pca",
-                              grouping.var = "orig.ident", dims.use = 1:10)
-SSCs <- SubsetData(SSCs, subset.name = "var.ratio.pca",accept.low = 0.5) #15555 out of 15650
+MouseSkin <- CalcVarExpRatio(object = MouseSkin, reduction.type = "pca",
+                        grouping.var = "orig.ident", dims.use = 1:20)
+MouseSkin
+MouseSkin <- SubsetData(MouseSkin, subset.name = "var.ratio.pca",accept.low = 0.5) #10291 out of 10350
+MouseSkin
 
-mito.genes <- grep(pattern = "^mt-", x = rownames(x = SSCs@data), value = TRUE)
-percent.mito <- Matrix::colSums(SSCs@raw.data[mito.genes, ])/Matrix::colSums(SSCs@raw.data)
-SSCs <- AddMetaData(object = SSCs, metadata = percent.mito, col.name = "percent.mito")
-#SSCs <- ScaleData(object = SSCs, genes.use = genes.use, display.progress = FALSE, 
+mito.genes <- grep(pattern = "^mt-", x = rownames(x = MouseSkin@data), value = TRUE)
+percent.mito <- Matrix::colSums(MouseSkin@raw.data[mito.genes, ])/Matrix::colSums(MouseSkin@raw.data)
+MouseSkin <- AddMetaData(object = MouseSkin, metadata = percent.mito, col.name = "percent.mito")
+#MouseSkin <- ScaleData(object = MouseSkin, genes.use = genes.use, display.progress = FALSE, 
 #                         vars.to.regress = "percent.mito")
 #Now we can run a single integrated analysis on all cells!
-VlnPlot(object = SSCs, features.plot = c("nGene", "nUMI", "percent.mito"), nCol = 1)
-SSCs <- FilterCells(object = SSCs, subset.names = c("nGene", "percent.mito"), 
-                          low.thresholds = c(500, -Inf), high.thresholds = c(7000, 0.15))
+VlnPlot(object = MouseSkin, features.plot = c("nGene", "nUMI", "percent.mito"), nCol = 1)
+par(mfrow = c(2, 1))
+GenePlot(object = MouseSkin, gene1 = "nUMI", gene2 = "percent.mito", ylim = c(0, 0.6))
+GenePlot(object = MouseSkin, gene1 = "nUMI", gene2 = "nGene", ylim = c(0, 4000))
 
-par(mfrow = c(1, 2))
-GenePlot(object = SSCs, gene1 = "nUMI", gene2 = "percent.mito")
-GenePlot(object = SSCs, gene1 = "nUMI", gene2 = "nGene")
-
+MouseSkin <- FilterCells(object = MouseSkin, subset.names = c("nGene", "percent.mito"), 
+                         low.thresholds = c(500, -Inf), high.thresholds = c(3500, 0.20))
+GenePlot(object = MouseSkin, gene1 = "nUMI", gene2 = "percent.mito", 
+         xlim = c(0,20000), ylim = c(0, 0.6))
+GenePlot(object = MouseSkin, gene1 = "nUMI", gene2 = "nGene", 
+         xlim = c(0,20000), ylim = c(0, 4000))
 #======1.4 align seurat objects =========================
 #Now we align the CCA subspaces, which returns a new dimensional reduction called cca.aligned
 set.seed(42)
-SSCs <- AlignSubspace(object = SSCs, reduction.type = "cca", grouping.var = "orig.ident", 
-                            dims.align = 1:10)
-SSCs@project.name <- "Paula"
+MouseSkin <- AlignSubspace(object = MouseSkin, reduction.type = "cca", grouping.var = "conditions", 
+                      dims.align = 1:20)
+MouseSkin@project.name <- "Fuchs"
 #Now we can run a single integrated analysis on all cells!
 
-SSCs <- FindClusters(object = SSCs, reduction.type = "cca.aligned", dims.use = 1:10, 
-                           resolution = 1.0, force.recalc = T, save.SNN = TRUE)
+MouseSkin <- FindClusters(object = MouseSkin, reduction.type = "cca.aligned", dims.use = 1:20, 
+                     resolution = 1.0, force.recalc = T, save.SNN = TRUE)
 
-SSCs <- RunTSNE(object = SSCs, reduction.use = "cca.aligned", dims.use = 1:10, 
-                      do.fast = TRUE)
+MouseSkin <- RunTSNE(object = MouseSkin, reduction.use = "cca.aligned", dims.use = 1:20, 
+                do.fast = TRUE)
 
-p1 <- TSNEPlot(SSCs, do.return = T, pt.size = 1, group.by = "orig.ident")
-p2 <- TSNEPlot(SSCs, do.return = T, pt.size = 1, group.by = "ident")
+p1 <- TSNEPlot(MouseSkin, do.return = T, pt.size = 1, group.by = "orig.ident")
+p2 <- TSNEPlot(MouseSkin, do.return = T, pt.size = 1, group.by = "ident")
 #png('./output/TSNESplot_alignment.png')
 plot_grid(p1, p2)
 #dev.off()
-TSNEPlot(object = SSCs,do.label = T, group.by = "ident", 
+TSNEPlot(object = MouseSkin,do.label = T, group.by = "ident", 
          do.return = TRUE, no.legend = T,
          pt.size = 1,label.size = 8 )+
-        ggtitle("All samples")+
-        theme(text = element_text(size=20),     #larger text including legend title							
-              plot.title = element_text(hjust = 0.5)) #title in middle
+        ggtitle("TSNE plot of young and old mouse skin")+
+        theme(text = element_text(size=20),     							
+              plot.title = element_text(hjust = 0.5))
 
-#======1.5 RunPCA==================================
-SSCs <- FindVariableGenes(object = SSCs, mean.function = ExpMean, dispersion.function = LogVMR, 
-                         do.plot = FALSE)
-hv.genes <- head(rownames(SSCs@hvg.info), 1000)
-SSCs <- RunPCA(object = SSCs, pc.genes = hv.genes, pcs.compute = 100, do.print = TRUE, 
-              pcs.print = 1:5, genes.print = 5)
-PCElbowPlot(object = SSCs, num.pc = 100)
-PCHeatmap(SSCs, pc.use = c(1:3, 25:30), cells.use = 500, do.balanced = TRUE)
-SSCs <- FindClusters(object = SSCs, reduction.type = "pca", dims.use = 1:30, resolution = 1, 
-                    save.SNN = TRUE, n.start = 10, nn.eps = 0.5, print.output = FALSE)
-SSCs <- RunTSNE(object = SSCs, reduction.use = "pca", dims.use = 1:30, 
-                do.fast = TRUE)
-TSNEPlot(object = SSCs,do.label = T, group.by = "orig.ident", 
-         do.return = TRUE, no.legend = T,
-         pt.size = 1,label.size = 8 )+
-        ggtitle("All samples")+
-        theme(text = element_text(size=15),     #larger text including legend title							
-              plot.title = element_text(hjust = 0.5)) 
-
-save(SSCs, file = "./data/SSCs_alignment.Rda")
+save(MouseSkin, file = "./data/MouseSkin_alignment.Rda")
--- R/SingleR.R
+++ R/SingleR.R
@@ -4,40 +4,38 @@ library(reshape2)
 library(pheatmap)
 library(kableExtra)
 source("../R/Seurat_functions.R")
+source("../R/SingleR_functions.R")
 
-#====== 3.1 Create Singler Object  ==========================================
-lnames = load(file = "./data/SSCs_label.Rda")
-lnames
-lname = load(file='./data/GeneSets/Ref_GSE43717.RData') 
-lname
-SSCs@meta.data$orig.ident = gsub("PND18pre","PND18",SSCs@meta.data$orig.ident)
-Ref_GSE43717$name
-length(Ref_GSE43717$types)
-length(unique(Ref_GSE43717$types))
-length(unique(Ref_GSE43717$main_types))
-length(Ref_GSE43717$sd.thres)
+#====== 2.1 Create Singler Object  ==========================================
+lname1 = load(file = "./data/MouseSkin_alignment.Rda");lname1
+lname2 = load(file='../SingleR/data/ref_Mouse.RData');lname2
+ref_immgen_mouse.rnaseq$name
+length(ref_immgen_mouse.rnaseq$types)
+length(unique(ref_immgen_mouse.rnaseq$types))
+length(unique(ref_immgen_mouse.rnaseq$main_types))
+
 #pca
-DimPlot(object = SSCs, reduction.use = "tsne", no.legend = TRUE,
+DimPlot(object = MouseSkin, reduction.use = "tsne", no.legend = TRUE,
         do.return = TRUE,vector.friendly = F, pt.size = 1,
         do.label = TRUE,label.size = 8, group.by = "ident") + 
         ggtitle("Cluster ID") + 
         theme(plot.title = element_text(hjust = 0.5))
-singler = CreateSinglerObject(as.matrix(SSCs@data), annot = NULL, 
-                              project.name=SSCs@project.name,
+singler = CreateSinglerObject(as.matrix(MouseSkin@data), annot = NULL, 
+                              project.name=MouseSkin@project.name,
                               min.genes = 500,technology = "10X", species = "Mouse",
-                              ref.list = list(GSE43717_GSE83264), normalize.gene.length = F, 
+                              ref.list = list(ref_immgen_mouse.rnaseq), normalize.gene.length = F, 
                               variable.genes = "de",
                               fine.tune = F, do.signatures = F, clusters = NULL)
 GC()
-singler$meta.data$orig.ident = SSCs@meta.data$orig.ident # the original identities, if not supplied in 'annot'
-singler$meta.data$xy = SSCs@dr$tsne@cell.embeddings # the tSNE coordinates
-singler$meta.data$clusters = SSCs@ident # the Seurat clusters (if 'clusters' not provided)
-save(singler,file="./output/singler_SSCs.RData")
+singler$meta.data$orig.ident = MouseSkin@meta.data$orig.ident # the original identities, if not supplied in 'annot'
+singler$meta.data$xy = MouseSkin@dr$tsne@cell.embeddings # the tSNE coordinates
+singler$meta.data$clusters = MouseSkin@ident # the Seurat clusters (if 'clusters' not provided)
+save(singler,file="./output/singler_MouseSkin.RData")
 #====== 3.2 SingleR specifications ==========================================
 # Step 1: Spearman coefficient
-lnames = load(file = "./output/singler_Ref_GSE43717.RData")
+lnames = load(file = "./output/singler_MouseSkin.RData")
 lnames
-singler$seurat = SSCs # (optional)
+singler$seurat = MouseSkin # (optional)
 SingleR.DrawScatter(sc_data = singler$seurat@data,cell_id = 10, 
                     ref = immgen, sample_id = 232)
 
@@ -62,23 +60,29 @@ SingleR.DrawHeatmap(singler$singler[[1]]$SingleR.single.main,top.n = 50,
 out = SingleR.PlotTsne.1(singler$singler[[1]]$SingleR.single,
                          singler$meta.data$xy,do.label=T,
                          do.letters = F,labels = singler$singler[[1]]$SingleR.single$labels,
-                         label.size = 5, dot.size = 1,do.legend = F,alpha = 1,
+                         label.size = 2, dot.size = 2 ,do.legend = F,alpha = 1,
                          label.repel = T,force=2)
-out+  ggtitle("Supervised sub-cell type labeling by immgen, GSE43717 and GSE83264")+
+out+  ggtitle("Supervised sub-cell type labeling by immgen")+
         theme(text = element_text(size=20),
               plot.title = element_text(hjust = 0.5,size = 18, face = "bold"))
 # main types-------
 out = SingleR.PlotTsne.1(singler$singler[[1]]$SingleR.single.main,
                          singler$meta.data$xy,do.label=T,
                          do.letters = F,labels = singler$singler[[1]]$SingleR.single.main$labels,
-                         label.size = 5, dot.size = 1,do.legend = F,alpha = 1,
+                         label.size = 5, dot.size = 2 ,do.legend = F,alpha = 1,
                          label.repel = T,force=2)
-out$p+  ggtitle("Supervised cell type labeling by immgen and GSE43717")+
+out +  ggtitle("Supervised cell type labeling by immgen and RNA-seq")+
         theme(text = element_text(size=20),
               plot.title = element_text(hjust = 0.5,size = 18, face = "bold"))
 
 g <- ggplot_build(out$p)
 
+# split singleR plot
+output <- SplitSingleR.PlotTsne(singler = singler, split.by = "conditions",main=T,
+                                select.plots =c(2,1),
+                                return.plots= T,do.label=T,do.legend = F,alpha = 1,
+                                label.repel = T, force=2)
+plot_grid(output[[1]], output[[2]])
 #Finally, we can also view the labeling as a table compared to the original identities:
 
 # cell number
@@ -87,23 +91,23 @@ kable(table(singler$singler[[1]]$SingleR.single.main$labels,
         kable_styling()
 # cell percentage
 prop.table(x = table(singler$singler[[1]]$SingleR.single.main$labels,
-                     SSCs@meta.data$orig.ident),margin = 2) %>%
+                     MouseSkin@meta.data$orig.ident),margin = 2) %>%
         kable  %>% kable_styling()
 # total cell number
 table(singler$meta.data$orig.ident) %>% t() %>% kable() %>% kable_styling()
 
 # Rename ident
-table(names(SSCs@ident) == rownames(singler$singler[[1]]$SingleR.single.main$labels))
+table(names(MouseSkin@ident) == rownames(singler$singler[[1]]$SingleR.single.main$labels))
 
 ident.use <- as.factor(as.character(singler$singler[[1]]$SingleR.single.main$labels))
 names(ident.use) = rownames(singler$singler[[1]]$SingleR.single.main$labels)
-SSCs@ident <- ident.use
+MouseSkin@ident <- ident.use
 
-TSNEPlot(object = SSCs,do.label = F, group.by = "ident", 
+TSNEPlot(object = MouseSkin,do.label = F, group.by = "ident", 
          do.return = TRUE, no.legend = F,
          pt.size = 1,label.size = 8 )+
         ggtitle("Supervised cell type labeling by GSE43717")+
         theme(text = element_text(size=20),							
               plot.title = element_text(hjust = 0.5))
 
-save(SSCs, file = "./data/SSCs_suplabel_GSE43717.Rda")
+save(MouseSkin, file = "./data/MouseSkin_suplabel_GSE43717.Rda")
